37c70231eb56dc02f6707d55e42ef647a8934361
unknown <>
2017-04-21 11:16:49 +0900

    Javaオブジェクト指向のクラス作成の課題提出します。

---
+++ CampChallenge_java/CampChallenge_java/005.オブジェクト指向１/sample.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kurikura
+ */
+public class sample {
+    public int a,b;
+    public void sample1(){
+        a=1;
+        b=3;
+    }
+    public void sample2(){
+        System.out.println(a);
+        System.out.println(b);
+    }    
+}
+
+ class sample1{
+  public static void main(String[] args){
+
+}
+}
